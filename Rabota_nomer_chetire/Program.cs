60e4855d9a54ce1e41580b954faf0f3b6b0a1fd6
evgenii <136128227+evgeniikruglikov@users.noreply.github.com>
2023-11-22 17:29:52 +0300

    Add files via upload

---
+++ Rabota_nomer_chetire/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace rabotanomerchetire
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a < b)
+            {
+                if (c > b)
+                    Console.WriteLine("{0}, {1}, {2}", c, b, a);
+
+                else
+                {
+                    if (a < c)
+                        Console.WriteLine("{0}, {1}, {2}", b, c, a);
+
+                    else
+                        Console.WriteLine("{0}, {1}, {2}", b, a, c);
+                }
+            }
+
+            else
+            {
+                if (c < b)
+                    Console.WriteLine("{0}, {1}, {2}", a, b, c);
+                else
+                {
+                    if (a < c)
+                        Console.WriteLine("{0}, {1}, {2}", c, a, b);
+
+                    else
+                        Console.WriteLine("{0}, {1}, {2}", a, c, b);
+
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
