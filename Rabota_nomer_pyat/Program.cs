db8a49bfb3006be0ba3b753d45d6461dc45b3d80
evgenii <136128227+evgeniikruglikov@users.noreply.github.com>
2023-12-03 15:42:13 +0300

    Add files via upload

---
+++ Rabota_nomer_pyat/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rabota_nomer_pyat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите символ арифметического оператора:");
+            string input = Console.ReadLine();
+
+            
+            char oper = input[0];
+            switch (oper)
+            {
+                case '+':
+                    Console.WriteLine("Символ '+' соответствует оператору сложения.");
+                    break;
+                case '-':
+                    Console.WriteLine("Символ '-' соответствует оператору вычитания.");
+                    break;
+                case '*':
+                    Console.WriteLine("Символ '*' соответствует оператору умножения.");
+                    break;
+                case '/':
+                    Console.WriteLine("Символ '/' соответствует оператору деления.");
+                    break;
+                case '%':
+                    Console.WriteLine("Символ '%' соответствует оператору остатка от деления.");
+                    break;
+                default:
+                    Console.WriteLine("Введен некорректный символ оператора.");
+                    break;
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
